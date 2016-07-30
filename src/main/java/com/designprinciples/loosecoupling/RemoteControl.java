f63765bb3f280cc513c6ea169926efd8af637d51
Mesut Yakut <mst@mesutyakut.com>
2016-07-30 08:14:05 +0300

    Remote Control has been edited for Loose Coupling Principle

--- src/main/java/com/designprinciples/loosecoupling/RemoteControl.java
+++ src/main/java/com/designprinciples/loosecoupling/RemoteControl.java
@@ -19,3 +19,34 @@ public class RemoteControl {
         remoteControl.off();
     }
 }
+
+/*
+    Before Loose Coupling  Principle
+    Our class was like below class
+    and it was useless.
+    If we would like to add some approach to program
+    We had to change the class
+ */
+
+/*
+public class RemoteControl {
+
+    private Tv tv = new Tv();
+    private Radio radio = new Radio();
+
+    public void onActionTv() {
+        tv.on();
+    }
+
+    public void offActionRadio() {
+        tv.off();
+    }
+
+    public void onActionRadio() {
+        radio.on();
+    }
+
+    public void offActionRadio() {
+        radio.off();
+    }
+}*/
